3082c6ccf9e0c21e95383944c302c70e5f7987b9
Mahrozfatima786 <mahrozfatima156@gmail.com>
2025-06-28 11:59:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
   // your code here
+	var firstWord = s.replace(/ .*/,'');
+
+$('body').append(firstWord);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String: see and stop");
 alert(firstWord(s));
